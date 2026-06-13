26b6e141c8fdc42168b39e2b7d9ed95e5d3eb387
Saverio Catacchio <saveriocatacchio@gmail.com>
2026-06-13 18:42:09 +0200

    fix chiave supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://rgbqpybaeojhrbqgxtui.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnYnFweWJhZW9qaHJicWd4dHVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMDU4MTQsImV4cCI6MjA5NDY4MTgxNH0.cqhhIuV0WC4c1BzuiL-RJHSBmR-TwfgygqAB-DexUqg"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
